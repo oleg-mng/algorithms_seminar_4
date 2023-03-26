beecfbe7a67673a7df8e0cc3cd04c88532ed8de3
oleg <legik7127@gmail.com>
2023-03-26 18:55:02 +0600

    added method add

--- src/main/java/HashMap.java
+++ src/main/java/HashMap.java
@@ -1,4 +1,3 @@
-import org.w3c.dom.Node;
 
 public class HashMap {
     private static final int INIT_SIZE = 16;
@@ -12,11 +11,11 @@ public class HashMap {
         baskets = new Basket[size];
     }
 
-    public int calcindex(int key) {
+    public int calcIndex(int key) {
         return key % baskets.length;
     }
     public Entity find(int key){
-        int index = calcindex(key);
+        int index = calcIndex(key);
         Basket basket = baskets[index];
         if (basket != null){
             return basket.find(key);
@@ -28,6 +27,17 @@ public class HashMap {
         int key;
         int value;
     }
+    public boolean add(int key, int value){
+        int index = calcIndex(key);
+        Basket basket = baskets[index];
+        if (basket == null){
+            basket = new Basket();
+        }
+        Entity entity = new Entity();
+        entity.key = key;
+        entity.value = value;
+        return basket.add(entity);
+    }
 
     private class Basket {
         Node head;
@@ -36,6 +46,37 @@ public class HashMap {
             Entity value;
             Node next;
         }
-        public Entity find(int key)
+        public Entity find(int key){
+            Node node = head;
+            while (node != null){
+                if (node.value.key == key) {
+                    return node.value;
+                }
+                node = node.next;
+            }
+            return null;
+        }
+        public boolean add(Entity entity){
+            Node node = new Node();
+            node.value = entity;
+            if (head == null){
+                head = node;
+            }
+            else {
+                Node current = head;
+                while (current.next != null){
+                    if (current.value.key == entity.key){
+                        return false;
+                    }
+                    current = current.next;
+                }
+                if (current.value.key == entity.key){
+                    return false;
+                }
+                current.next = node;
+            }
+            return true;
+        }
+
     }
 }
