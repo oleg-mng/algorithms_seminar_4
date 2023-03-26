ec125d486b49170977396a70366e56b2c07d4b0d
oleg <legik7127@gmail.com>
2023-03-26 17:47:03 +0600

    added class Basket

--- src/main/java/HashMap.java
+++ src/main/java/HashMap.java
@@ -1,6 +1,14 @@
 import org.w3c.dom.Node;
 
 public class HashMap {
+    private static final int INIT_SIZE=16;
+    Basket[] baskets;
+    public HashMap{
+        this(INIT_SIZE);
+    }
+    public HashMap(int size){
+        baskets = new Basket[size];
+    }
 
     private class Entity {
         int key;
